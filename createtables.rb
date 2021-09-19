7dda35a61c143cc75b66ca9fec1017028ec66c3e
Patrik Fältström <paf@frobbit.se>
2021-09-19 17:41:39 +0200

    Remove dependency of libraries that we do not use.

--- createtables.rb
+++ createtables.rb
@@ -38,9 +38,6 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-require 'rubygems'
-require 'idn'
-
 def stringToArray(s)
   if(s.class == Array)
     return(s)
